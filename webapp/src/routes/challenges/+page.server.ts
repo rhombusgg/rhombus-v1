2853c842628a39b4eb75aadc68733a51407fec6b
Mark Bundschuh <bundschuhmark@gmail.com>
2023-11-24 02:43:23 -0500

    board diffing

--- webapp/src/routes/challenges/+page.server.ts
+++ webapp/src/routes/challenges/+page.server.ts
@@ -34,7 +34,91 @@ export const load = async ({ locals }) => {
 		.map((challenge) => challenge.category)
 		.filter((v, i, a) => a.indexOf(v) === i);
 
-	let columns = await prisma.userColumns.findMany({
+	const board = await prisma.userColumns.findMany({
+		where: { userId: locals.session.id },
+		select: {
+			challenges: { select: { challengeId: true } },
+			name: true,
+			id: true
+		}
+	});
+	const newCategories = categories.filter(
+		(category) => !board.some((column) => column.name === category)
+	);
+	const deletedCategories = board.filter(
+		(column) => !categories.some((category) => category === column.name)
+	);
+	const userChallenges = board.flatMap((column) => column.challenges);
+	const newChallenges = dbChallenges.filter(
+		(challenge) =>
+			!userChallenges.some((userChallenge) => userChallenge.challengeId === challenge.id)
+	);
+	const deletedChallenges = userChallenges.filter(
+		(userChallenge) => !dbChallenges.some((challenge) => challenge.id === userChallenge.challengeId)
+	);
+
+	// Create new categories
+	await prisma.userColumns.createMany({
+		data: newCategories.map((category) => ({
+			userId: locals.session!.id,
+			name: category,
+			order: 0
+		}))
+	});
+	const updatedColumns = await prisma.userColumns.findMany({
+		where: { userId: locals.session.id },
+		select: {
+			name: true,
+			id: true
+		}
+	});
+
+	// Manage challenges
+	await prisma.user.update({
+		where: { id: locals.session.id },
+		data: {
+			userChallenges: {
+				createMany: {
+					data: newChallenges.map((challenge) => ({
+						challengeId: challenge.id,
+						columnId: updatedColumns.find((column) => column.name === challenge.category)!.id,
+						order: 0
+					}))
+				},
+				updateMany: deletedCategories.flatMap((column) =>
+					column.challenges.flatMap((challenge) => {
+						const category = dbChallenges.find((chall) => chall.id === challenge.challengeId)
+							?.category;
+						if (!category) return [];
+						return {
+							where: { challengeId: challenge.challengeId },
+							data: {
+								columnId: board.find((c) => c.name === category)!.id
+							}
+						};
+					})
+				),
+				deleteMany: deletedChallenges.map((userChallenge) => ({
+					challengeId: userChallenge.challengeId,
+					userId: locals.session!.id
+				}))
+			}
+		},
+		select: {
+			id: true
+		}
+	});
+
+	// Delete old categories
+	await prisma.userColumns.deleteMany({
+		where: {
+			userId: locals.session.id,
+			name: { in: deletedCategories.map((column) => column.name) }
+		}
+	});
+
+	// Get final board
+	const columns = await prisma.userColumns.findMany({
 		where: { userId: locals.session.id },
 		select: {
 			challenges: { select: { challengeId: true }, orderBy: { order: 'asc' } },
@@ -43,36 +127,6 @@ export const load = async ({ locals }) => {
 		},
 		orderBy: { order: 'asc' }
 	});
-
-	if (!columns || columns.length === 0) {
-		const user = await prisma.user.update({
-			where: { id: locals.session.id },
-			data: {
-				columns: {
-					create: categories.map((category, i) => ({
-						name: category,
-						order: i,
-						challenges: {
-							createMany: {
-								data: dbChallenges
-									.filter((challenge) => challenge.category === category)
-									.map((challenge, j) => ({
-										challengeId: challenge.id,
-										order: j,
-										userId: locals.session!.id
-									}))
-							}
-						}
-					}))
-				}
-			},
-			select: {
-				columns: { select: { challenges: { select: { challengeId: true } }, name: true, id: true } }
-			}
-		});
-
-		columns = user.columns;
-	}
 
 	const userColumns = columns.map((column) => ({
 		id: column.id,
