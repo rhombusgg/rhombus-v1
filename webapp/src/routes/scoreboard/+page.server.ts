1c5e325ea65519377d98b7daa014bc265cc42453
Mark Bundschuh <bundschuhmark@gmail.com>
2023-12-16 01:43:07 -0500

    better scoreboard

--- webapp/src/routes/scoreboard/+page.server.ts
+++ webapp/src/routes/scoreboard/+page.server.ts
@@ -4,6 +4,7 @@ export const load = async () => {
 	const dbTeams = await prisma.team.findMany({
 		select: {
 			name: true,
+			id: true,
 			users: {
 				select: {
 					solves: {
@@ -19,6 +20,7 @@ export const load = async () => {
 
 	const teams = dbTeams.map((team) => ({
 		name: team.name,
+		id: team.id,
 		solves: team.users.flatMap((user) =>
 			user.solves.map((solve) => ({
 				time: solve.time,
@@ -29,34 +31,15 @@ export const load = async () => {
 
 	const teamScores = teams.map((team) => ({
 		name: team.name,
+		id: team.id,
+		score: team.solves.reduce((acc, solve) => acc + solve.points, 0),
 		solves: team.solves.map((solve, i) => ({
 			time: solve.time,
 			points: solve.points + (team.solves[i - 1]?.points || 0)
 		}))
 	}));
 
-	const series = teamScores.map((team) => ({
-		type: 'line',
-		name: team.name,
-		data: team.solves.map((solve) => ({
-			value: [solve.time.getTime(), solve.points]
-		}))
-	}));
-
-	// const series = teamScores.map((team) => ({
-	// 	type: 'line',
-	// 	name: team.name,
-	// 	data: [
-	// 		{
-	// 			value: [Date.now(), 100]
-	// 		},
-	// 		{
-	// 			value: [Date.now(), 200]
-	// 		}
-	// 	]
-	// }));
-
 	return {
-		series
+		teamScores
 	};
 };
--- webapp/src/routes/scoreboard/+page.svelte
+++ webapp/src/routes/scoreboard/+page.svelte
@@ -1,10 +1,29 @@
 <script lang="ts">
+	import { invalidateAll } from '$app/navigation';
 	import { Chart, type EChartsOptions } from '$lib/components/charts';
+	import type { LineSeriesOption } from 'echarts';
 
 	export let data;
-	console.log(data.series);
 
-	const options: EChartsOptions = {
+	let series: LineSeriesOption[];
+	$: series = data.teamScores.map((team) => ({
+		type: 'line',
+		name: team.name,
+		data: team.solves.map((solve) => ({
+			value: [solve.time.getTime(), solve.points]
+		})),
+		emphasis: {
+			focus: 'series'
+		},
+		lineStyle: {
+			width: 4
+		},
+		symbolSize: 20,
+		smooth: true
+	}));
+
+	let options: EChartsOptions;
+	$: options = {
 		title: {
 			text: 'Scoreboard'
 		},
@@ -15,11 +34,27 @@
 			type: 'time'
 		},
 		yAxis: {
-			type: 'value'
+			type: 'value',
+			min: 0,
+			axisLabel: {
+				formatter: '{value} pts'
+			}
 		},
-		series: data.series
+		toolbox: {
+			feature: {
+				saveAsImage: {}
+			}
+		},
+		series
 	};
 </script>
+
+<svelte:head>
+	<title>Scoreboard</title>
+	<meta name="description" content="Scoreboard" />
+</svelte:head>
+
+<svelte:document on:focus={() => invalidateAll()} />
 
 <div class="w-full">
 	<Chart {options} />
